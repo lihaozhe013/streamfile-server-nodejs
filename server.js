af45e271796e61b2a0c97276683077855dfaa49f
Haozhe Li <lihaozhe013@outlook.com>
2025-05-15 15:55:08 +0800

    fix: console message adjustment

--- server.js
+++ server.js
@@ -136,5 +136,5 @@ app.get('/', (req, res) => {
 
 // start
 app.listen(PORT, HOST, () => {
-    console.log(`NAS Started: http://${HOST === '0.0.0.0' ? LOCAL_IP : HOST}:${PORT}`);
+    console.log(`Server Started: http://${HOST === '0.0.0.0' ? LOCAL_IP : HOST}:${PORT}`);
 });
