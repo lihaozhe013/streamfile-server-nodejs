c4863b0a08d852ff97c187de657806f73c26e776
Haozhe Li <lihaozhe013@outlook.com>
2025-06-08 23:08:04 +0800

    fix: fixed child page ui bugs

--- public/file-browser/file-browser.js
+++ public/file-browser/file-browser.js
@@ -1,14 +1,11 @@
 function getCurrentDirectoryPath() {
-  const urlParams = new URLSearchParams(window.location.search);
-  const queryPath = urlParams.get('path');
-  if (queryPath !== null) return decodeURIComponent(queryPath);
-
-  const prefix = '/files/';
+  // Remove '/files' prefix and any leading/trailing slashes
   let pathname = window.location.pathname;
-  if (pathname.startsWith(prefix)) {
-      let path = pathname.substring(prefix.length);
-      if (path.endsWith('/')) path = path.slice(0, -1);
-      return decodeURIComponent(path);
+  if (pathname.startsWith('/files')) {
+    let path = pathname.substring('/files'.length);
+    if (path.startsWith('/')) path = path.substring(1);
+    if (path.endsWith('/')) path = path.slice(0, -1);
+    return decodeURIComponent(path);
   }
   return '';
 }
@@ -20,69 +17,58 @@ async function fetchFiles(path = '') {
   const list = document.getElementById('file-list');
   list.innerHTML = '';
 
+  // Navigation row
   if (path) {
-      const up = path.split('/').slice(0, -1).join('/');
-      let upUrl = '/files/';
-      if (up) upUrl += encodeURIComponent(up) + '/';
-      list.innerHTML += 
+    const up = path.split('/').slice(0, -1).join('/');
+    let upUrl = '/files';
+    if (up) upUrl += '/' + encodeURIComponent(up).replace(/%2F/g, '/');
+    if (!upUrl.endsWith('/')) upUrl += '/';
+    list.innerHTML += 
       `<a href="${upUrl}" class="file-row">
         <img src="/icons/icons8-folder-48.png" class="icons">
         (Go Back)
-      </a> <br>`;
+      </a>`;
   } else {
-      list.innerHTML += 
-      `<a href="/files/" class="file-row">
+    list.innerHTML += 
+      `<a href="/" class="file-row">
         <img src="/icons/icons8-folder-48.png" class="icons">
-        (Go Back)
+        (Back to Home)
       </a>`;
   }
 
-  files.forEach(async file => {
-      const isDir = file.isDirectory;
-      const fullPath = path ? `${path}/${file.name}` : file.name;
-      const displayName = file.name;
-      const encodedPath = fullPath.split('/').map(encodeURIComponent).join('/');
+  files.forEach(file => {
+    const isDir = file.isDirectory;
+    const fullPath = path ? `${path}/${file.name}` : file.name;
+    const displayName = file.name;
+    const encodedPath = fullPath.split('/').map(encodeURIComponent).join('/');
 
-      if (displayName === '.DS_Store') return;
+    if (displayName === '.DS_Store') return;
 
-      if (isDir) {
-          const indexUrl = `/files/${encodedPath}/index.html`;
-          try {
-              const res = await fetch(indexUrl, { method: 'HEAD' });
-              if (res.ok) {
-                  const href = `/files/${encodedPath}/`;
-                  list.innerHTML += `
-                      <a href="${href}" class="file-row">
-                          <img src="/icons/icons8-folder-48.png" class="icons">
-                          ${displayName}
-                      </a>`;
-                  return;
-              }
-          } catch (e) {}
-
-          const href = `/files/${encodedPath}/`;
-          list.innerHTML += `
-              <a href="${href}" class="file-row">
-                  <img src="/icons/icons8-folder-48.png" class="icons">
-                  ${displayName}
-              </a>`;
-      } else {
-          const filePath = `/files/${encodedPath}`;
-          list.innerHTML += `
-              <a href="${filePath}" class="file-row">
-                  <img src="/icons/file-icon.png" class="icons">
-                  ${displayName}
-              </a>`;
-      }
+    if (isDir) {
+      const href = `/files/${encodedPath}/`;
+      list.innerHTML += `
+        <a href="${href}" class="file-row">
+          <img src="/icons/icons8-folder-48.png" class="icons">
+          ${displayName}
+        </a>`;
+    } else {
+      const filePath = `/files/${encodedPath}`;
+      list.innerHTML += `
+        <a href="${filePath}" class="file-row">
+          <img src="/icons/file-icon.png" class="icons">
+          ${displayName}
+        </a>`;
+    }
   });
 
+  // Add click event for folder navigation (SPA-like)
   list.querySelectorAll('a.file-row').forEach(a => {
-      if (a.getAttribute('href').startsWith('/files/') && a.getAttribute('href').endsWith('/')) {
-          a.addEventListener('click', function (e) {
-              e.preventDefault();
-              window.location.href = a.getAttribute('href');
-          });
-      }
+    if (a.getAttribute('href').startsWith('/files/') && a.getAttribute('href').endsWith('/')) {
+      a.addEventListener('click', function (e) {
+        e.preventDefault();
+        window.location.href = a.getAttribute('href');
+      });
+    }
   });
 }
 
